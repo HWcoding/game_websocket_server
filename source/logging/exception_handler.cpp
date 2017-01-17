514b1cd03a28ce34159ce65b56eab5306da7951b
Herman Workman <hworkman@hwcoding.com>
2017-01-17 01:08:11 -0500

    added multithread support to backtrace

--- source/logging/exception_handler.cpp
+++ source/logging/exception_handler.cpp
@@ -7,18 +7,33 @@
 #include <memory>
 #include <cxxabi.h>
 #include <cstdlib>
+#include <thread>
+#include <map>
+#include <mutex>
 
 namespace {
 
 const int frameSize = 100;
-void * last_frames[frameSize];
-int last_size = 0;
-std::string exception_name;
+struct ExceptionInfo
+{
+	void* last_frames[frameSize];
+	int last_size {0};
+	std::string exception_name {};
+};
+
+std::mutex mut;
+std::map<std::thread::id, ExceptionInfo> frames;
+
 std::string demangle(const char *name)
 {
 	int status;
 	std::unique_ptr<char,void(*)(void*)> realname(abi::__cxa_demangle(name, 0, 0, &status), &std::free);
-	return status ? std::string("failed") : std::string( realname.get() );
+	if(status != 0){
+		return std::string("unknown");
+	}
+	else {
+		return std::string( realname.get() );
+	}
 }
 
 } // namespace
@@ -28,13 +43,18 @@ namespace DEBUG_BACKTRACE{
 
 void debugBackTrace()
 {
-	std::unique_ptr<char*,void(*)(void*)> backTraceArray( backtrace_symbols(::last_frames, ::last_size), &std::free);
+	std::unique_lock<std::mutex> lck(::mut);
+
+	std::thread::id this_id = std::this_thread::get_id();
+	if(frames.count(this_id)==0) return; //nothing to print
+
+	std::unique_ptr<char*,void(*)(void*)> backTraceArray( backtrace_symbols(::frames[this_id].last_frames, ::frames[this_id].last_size), &std::free);
 	if( backTraceArray.get() != nullptr ){
 		std::string debugBacktrace;
-		int i = ::last_size;
+		int i = ::frames[this_id].last_size;
 		while(--i > 0) { // i == 0 is __cxa_throw
 			Dl_info info;
-			dladdr(::last_frames[i], &info);
+			dladdr(::frames[this_id].last_frames[i], &info);
 
 			int status;
 			std::unique_ptr<char,void(*)(void*)> demangled( abi::__cxa_demangle(info.dli_sname, nullptr, 0, &status), &std::free);
@@ -50,8 +70,9 @@ void debugBackTrace()
 			debugBacktrace.insert(0,functionInfo);
 
 		}
-		LOG_ERROR("Exception of type "<<exception_name<<" caught. Stack trace: \n"<<debugBacktrace);
+		LOG_ERROR("Exception of type "<<frames[this_id].exception_name<<" caught. Stack trace: \n"<<debugBacktrace);
 	}
+	frames.erase(this_id);
 }
 
 } // DEBUG_BACKTRACE
@@ -63,14 +84,16 @@ extern "C" {
 void __cxa_throw(void *ex, void *info, void (*dest)(void *))
 {
 	//save exception name and backtrace in globals
-	//TODO: there is a race condition if a second thread
-	//throws before DEBUG_BACKTRACE::debugBackTrace() completes
-	::exception_name = ::demangle(reinterpret_cast<const std::type_info*>(info)->name());
-	::last_size = backtrace(::last_frames, ::frameSize);
+	{ //lock
+	std::unique_lock<std::mutex> lck(::mut);
+	std::thread::id this_id = std::this_thread::get_id();
+	::frames[this_id].exception_name = ::demangle(reinterpret_cast<const std::type_info*>(info)->name());
+	::frames[this_id].last_size = backtrace(::frames[this_id].last_frames, ::frameSize);
+	} //unlock
 
 	// get a pointer to the real __cxa_throw function
 	static void (*const real_cxa_throw)(void*,void*,void(*)(void*)) __attribute__ ((noreturn)) = (void (*)(void*,void*,void(*)(void*)))dlsym(RTLD_NEXT, "__cxa_throw");
-	// call the function so it can do default behavior
+	// call the real __cxa_throw so it can do default behavior
 	real_cxa_throw(ex,info,dest);
 }
 
